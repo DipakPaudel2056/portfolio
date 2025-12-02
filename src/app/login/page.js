6b539d63f39b176b67f1e4318be94df7301e3e04
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-12-02 13:00:08 +0800

    added suspense boundry in login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,44 +3,53 @@
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const Page = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const params = useSearchParams();
-  const error = params.get("error");
-  // handle submit
-  async function handleSubmit(event) {
-    event.preventDefault();
-    await signIn("credentials", {
-      email,
-      password,
-      callbackUrl: "/dashboard",
-    });
-  }
+  const Loginform = () => {
+    "use client";
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const params = useSearchParams();
+    const error = params.get("error");
+    // handle submit
+    async function handleSubmit(event) {
+      event.preventDefault();
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/dashboard",
+      });
+    }
+    return (
+      <div className="main">
+        welcome to login page
+        {error === "CredentialsSignin" && (
+          <p className="error">Invalid email or password.</p>
+        )}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email"
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+          />
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  };
 
   return (
-    <div className="main">
-      welcome to login page
-      {error === "CredentialsSignin" && (
-        <p className="text-red-500">Invalid email or password.</p>
-      )}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email"
-        />
-        <input
-          type="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="password"
-        />
-        <button type="submit">login</button>
-      </form>
-    </div>
+    <Suspense fallback={<>loading search params...</>}>
+      <Loginform />
+    </Suspense>
   );
 };
 
