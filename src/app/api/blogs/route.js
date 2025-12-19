781d8ec0165bc61837911679b50b6ff19b0c277a
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-12-19 08:44:52 +0800

    making a runtime to nodejs to bypass vercel gotcha

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,6 +1,9 @@
-import { redirect } from "next/dist/server/api-utils";
+export const runtime = "nodejs";
+
+import { redirect } from "next/navigation";
 import prisma from "../../lib/prisma";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -8,36 +11,40 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
+
 export async function POST(req) {
-  // get the form fields
   const formData = await req.formData();
   const image = formData.get("image");
-  const buffer = Buffer.from(await image.arrayBuffer());
-  // handle s3 image upload here
-  await s3Client.send(
-    new PutObjectCommand({
-      Bucket: process.env.PORTFOLIO_BUCKET_NAME,
-      Key: image.name,
-      Body: buffer,
-      ContentType: image.type,
-    })
-  );
-  const rawFormData = {
-    tag: formData.get("tag")?.toString() || "",
-    title: formData.get("title")?.toString() || "",
-    description: formData.get("description")?.toString() || "",
-    imageurl: `https://${process.env.PORTFOLIO_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${image.name}`,
-  };
-  // get prisma to post this
-  const newBlog = await prisma.blog.create({
-    data: rawFormData,
-  });
 
-  return new Response(
-    JSON.stringify({ message: "created new blog", newBlog }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    }
-  );
+  let imageurl = null;
+
+  if (image && image.size > 0) {
+    const buffer = Buffer.from(await image.arrayBuffer());
+
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.PORTFOLIO_BUCKET_NAME,
+        Key: image.name,
+        Body: buffer,
+        ContentType: image.type,
+      })
+    );
+
+    imageurl = `https://${process.env.PORTFOLIO_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${image.name}`;
+  }
+
+  const newBlog = await prisma.blog.create({
+    data: {
+      tag: formData.get("tag")?.toString() || "",
+      title: formData.get("title")?.toString() || "",
+      description: formData.get("description")?.toString() || "",
+      imageurl,
+    },
+  });
+  redirect("/blogs")
+
+  return new Response(JSON.stringify(newBlog), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
