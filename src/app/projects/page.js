72c71c3917dc853e2593b373f9c9835cbf460391
Dipak <paudelsantosh508@gmail.com>
2026-02-27 09:01:56 +0800

    some minor design change in the projects

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,9 @@
 import ProjectCard from "../ui/ProjectCard";
+import "./projects.style.css";
 const page = () => {
   return (
     <div className="main">
+      <div className="projects">
         <ProjectCard
           projectId={1}
           projectName="WordToHtmlApp"
@@ -10,16 +12,23 @@ const page = () => {
           timeTaken="2 hours"
           liveURL="https://wordtohtmlapp.vercel.app/"
           githubURL="https://github.com/DipakPaudel2056/wordtohtmlapp"
+          thumbnailPic={
+            "https://dipak-portfolio-asset.s3.ap-southeast-2.amazonaws.com/dipak_paudel_word_to_html.png"
+          }
         />
-         <ProjectCard
+        <ProjectCard
           projectId={2}
           projectName="Data cleaning with pandas"
-          projectDescription="I cleaned the messy csv dat using pandas."
-          techUsed={["Python","Pandas","numpy"]}
+          projectDescription="I cleaned the messy csv data using pandas."
+          techUsed={["Python", "Pandas", "numpy"]}
           timeTaken="0.5 hour"
           liveURL="https://github.com/DipakPaudel2056/panda-notebook"
           githubURL="https://github.com/DipakPaudel2056/panda-notebook"
+          thumbnailPic={
+            "https://dipak-portfolio-asset.s3.ap-southeast-2.amazonaws.com/dipak_paudel_data_cleaning.png"
+          }
         />
+      </div>
     </div>
   );
 };
